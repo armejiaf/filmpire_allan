83fc0261bfb2b1e96558da67ab5a193aeab6aae2
Allan Mejia <almesiete@gmail.com>
2023-11-03 00:32:29 -0600

    Added navbar and sidebar.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import reportWebVitals from 'reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import App from './components/App';
 
+const theme = createTheme({});
 // For React version 18 +
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(<App />);
 
 // Deprecated in React Version 18 +
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
   document.getElementById('root'),
 );
-
-// reportWebVitals();
