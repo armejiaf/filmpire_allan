3b953ff9d06df0b06c8f33b16584ba6ca246c356
Allan Mejia <almesiete@gmail.com>
2023-11-06 11:26:33 -0600

    Show popular movies.

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -3,15 +3,36 @@ import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material'
 import { useSelector } from 'react-redux';
 
 import { useGetPopularMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '..';
 
 const Movies = () => {
-  const { data } = useGetPopularMoviesQuery();
+  const { data, error, isFetching } = useGetPopularMoviesQuery();
 
-  console.log(data);
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'An error has occurred.';
 
   return (
     <div>
-      Movies
+      <MovieList movies={data} />
     </div>
   );
 };
