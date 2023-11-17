11393ad9357fb3b45d66036a56d1f87eea87c03e
Allan Mejia <almesiete@gmail.com>
2023-11-17 13:04:47 -0600

    Added alan ai voice funcionality.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
 
 import useStyles from './styles';
 import 'rsuite/dist/rsuite.min.css';
+import useAlan from './Alan';
 
 import { Actors, MovieInformation, Movies, NavBar, Profile } from '.';
 
 const App = () => {
   const styles = useStyles();
+  const alanBtnContainer = useRef();
+
+  useAlan();
 
   return (
     <div className={styles.root}>
@@ -24,6 +28,7 @@ const App = () => {
           <Route exact path="/actors/:id" element={<Actors />} />
         </Routes>
       </main>
+      <div ref={alanBtnContainer} />
     </div>
   );
 };
